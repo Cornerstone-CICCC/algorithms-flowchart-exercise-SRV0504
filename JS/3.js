6b46d9133d9351bfd924f8c0753deba38c3a7c0a
Samuel Rios <srios050402@gmail.com>
2025-05-22 17:43:03 -0700

    Javascript practice

---
+++ JS/3.js
@@ -0,0 +1,17 @@
+let A = 5
+let B = 24
+let C = 15
+
+if (A > B){
+    if (A > C){
+        console.log(A)
+    } else {
+        console.log(C)
+    }
+} else {
+    if (B > C) {
+        console.log(B)
+    } else {
+        console.log(C)
+    }
+}
