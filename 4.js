a0b5435de0de8e704b495e591e56553445b052fb
Samuel Rios <srios050402@gmail.com>
2025-05-22 17:44:02 -0700

    js exercise

---
+++ 4.js
@@ -0,0 +1,11 @@
+let A = 45
+let B = 45
+let C = 90 
+
+let sum = A + B + C
+
+if (sum === 180) {
+    console.log("Triangle is possible")
+  } else {
+    console.log("Triangle is not possible")
+  }
---
+++ 5.js
@@ -0,0 +1,15 @@
+
+let name = "John"
+let marks = 72
+
+
+if (marks >= 80) {
+  console.log(name + "'s Grade: A")
+} else if (marks >= 60 && marks < 80) {
+  console.log(name + "'s Grade: B")
+} else if (marks >= 40 && marks < 60) {
+  console.log(name + "'s Grade: C")
+} else {
+  console.log(name + "'s Grade: No grade")
+}
+
